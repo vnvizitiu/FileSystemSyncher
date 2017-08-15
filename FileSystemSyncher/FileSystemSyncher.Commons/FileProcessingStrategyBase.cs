3f315b6d25803335c026599cf6c34752c6d10294
Vizitiu Vlad Neculai <vnvizitiu@live.com>
2017-08-15 20:39:03 +0200

    Added base class for file processing;

---
+++ FileSystemSyncher/FileSystemSyncher.Commons/FileProcessingStrategyBase.cs
@@ -0,0 +1,12 @@
+﻿namespace FileSystemSyncher.Commons
+{
+    using System;
+    using System.IO;
+
+    public abstract class FileProcessingStrategyBase
+    {
+        public event EventHandler OnFileProcessed;
+
+        public abstract void ProcessFiles(FileInfo sourceFileInfo, FileInfo destinationFileInfo);
+    }
+}
